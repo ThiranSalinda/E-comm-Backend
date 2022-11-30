60bf4a175532206e0cadf533182d8acc49bffbfe
ThiranSalinda <thiransalinda.i@gmail.com>
2022-11-30 11:57:52 +0530

    Cart DeleteAll funtion updated Cart summary funtion added

--- models/Cart.js
+++ models/Cart.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
-const CartSchema = new mongoose.Schema(
+const CartSchema = Schema(
   {
-    productId: { type: String },
+    product: { type: Schema.Types.ObjectId, ref: "Product" },
     quantity: { type: Number, default: 1 },
   },
   { timeseries: true }
